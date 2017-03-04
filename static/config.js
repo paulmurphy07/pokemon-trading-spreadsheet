53d59c2e78ae8fc32b2651bfcf228e03ece88c2e
paulmurphy07 <paulmurphyquiambao07@gmail.com>
2017-03-03 20:05:53 -0800

    Change config values for Paul

--- static/config.js
+++ static/config.js
@@ -2,21 +2,21 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1-IydonSeICCvmbbXP0BxgZno7t5CJunur9NL39p1nTk";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
 var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var inGameName = "paulmurphy07";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "https://www.reddit.com/user/paulmurphy07/";
 
 // ***********************************************************************************
-// URL of your PGL trainer icon (optional)
+// URL of your PGL trainer icon (optionhttps://www.reddit.com/user/paulmurphy07/al)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "http://i.imgur.com/x0owLvW.png";
